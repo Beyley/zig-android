b93a70a844b0a01f2d738207fc3e3401c2afa980
Beyley Thomas <ep1cm1n10n123@gmail.com>
2023-05-31 13:29:47 -0700

    Export SDL_main in example project

--- src/example.zig
+++ src/example.zig
@@ -1,5 +1,1 @@
-export fn ANativeActivity_onCreate(activity: ?*anyopaque, saved_state: ?[*]u8, saved_state_size: usize) callconv(.C) void {
-    _ = saved_state_size;
-    _ = saved_state;
-    _ = activity;
-}
+export fn SDL_main() callconv(.C) void {}
